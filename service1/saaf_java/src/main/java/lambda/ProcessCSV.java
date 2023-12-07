7c5a963ac7546fb606ccf751c32b0d3dcda1f4a2
Brucegyx <yexuan.gao@outlook.com>
2023-12-07 15:01:53 -0800

    service1 update

--- service1/saaf_java/src/main/java/lambda/ProcessCSV.java
+++ service1/saaf_java/src/main/java/lambda/ProcessCSV.java
@@ -12,14 +12,25 @@ import com.amazonaws.services.s3.model.S3Object;
 import saaf.Inspector;
 import saaf.Response;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.io.ByteArrayInputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Stream;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 /**
  * uwt.lambda_test::handleRequest
@@ -27,30 +38,30 @@ import java.util.stream.Stream;
  * @author Wes Lloyd
  * @author Robert Cordingly
  */
-public class ProcessCSV implements RequestHandler<Request, HashMap<String, Object>> {
-
+public class ProcessCSV implements RequestHandler<HashMap<String, Object>, HashMap<String, Object>> {
+    private static AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
     /**
      * Lambda Function Handler
      * 
-     * @param request Request POJO with defined variables from Request.java
+     * @param request 
      * @param context 
      * @return HashMap that Lambda will automatically convert into JSON.
      */
-    public HashMap<String, Object> handleRequest(Request request, Context context) {
+    public HashMap<String, Object> handleRequest(HashMap<String, Object> request, Context context) {
         
         //Collect inital data.
         Inspector inspector = new Inspector();
-        
+        //lambda logger
+        LambdaLogger logger = context.getLogger();
         //****************START FUNCTION IMPLEMENTATION*************************
         //Add custom key/value attribute to SAAF's output. (OPTIONAL)
         // select csv file from s3 based on bucketname and filename
-        String bucketname = request.getBucketname();
-        String filename = request.getFilename();
-        int row = request.getRow();
-        int col = request.getCol();
+        String bucketname = request.get("bucketName").toString();
+        String filename = request.get("key").toString();
+
 
         //create new file on s3
-        AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
+        
         //get object file using bucket name and filename
         S3Object s3Object = s3Client.getObject(new GetObjectRequest(bucketname, filename));
         //get content of the file
@@ -61,15 +72,80 @@ public class ProcessCSV implements RequestHandler<Request, HashMap<String, Objec
        // write the transformed content to a new file on s3
         StringWriter sw = new StringWriter();
         Scanner scanner = new Scanner(objectData); 
+
+        HashSet<String> orderID = new HashSet<>();
+        // read the headers
+        String headerLine = scanner.nextLine();
+        //convert to list to find the index of required columns
+        List<String> header = Arrays.asList(headerLine.split(","));
+        // find  the index of an element in the array header
+        int orderPriorityIndex = header.indexOf("Order Priority");
+        int orderIdIndex = header.indexOf("Order ID");
+        int shipTimeIndex = header.indexOf("Ship Date");
+        int orderTimeIndex = header.indexOf("Order Date");
+        int totalProfitIndex = header.indexOf("Total Profit");
+        int totalRevenueIndex = header.indexOf("Total Revenue");
+        
+        // add 'order processing time' column to the header, and write to the new file
+        headerLine += ",Order Processing Time,Gross Margin\n";
+        sw.append(headerLine);
         while (scanner.hasNext()) {
             //String[] lineText = scanner.nextLine().split(",");
-            sw.append(scanner.nextLine() + "\n");
+            String line = scanner.nextLine();
+            String[] rowData = line.split(",");
+            // add 'order processing time' column
+            // create a date object for 'ship date' 
+            
+            try {
+                Date shipDate = new SimpleDateFormat("MM/dd/yyyy").parse(rowData[shipTimeIndex]);
+                Date orderDate = new SimpleDateFormat("MM/dd/yyyy").parse(rowData[orderTimeIndex]);
+                long diff = shipDate.getTime() - orderDate.getTime();
+                long diffDays = diff / (24 * 60 * 60 * 1000);
+                rowData = Arrays.copyOf(rowData, rowData.length + 1);
+                rowData[rowData.length - 1] = String.valueOf(diffDays);
+            } catch (ParseException e) {
+                Logger.getLogger(ProcessCSV.class.getName()).log(Level.SEVERE, null, e);
+            }
+            
+
+            // transform 'order priority' from letters to words
+            String orderPriority = rowData[orderPriorityIndex];
+            switch (orderPriority) {
+                case "L":
+                    orderPriority = "Low";
+                    break;
+                case "M":
+                    orderPriority = "Medium";
+                    break;
+                case "H":
+                    orderPriority = "High";
+                    break;
+                case "C":
+                    orderPriority = "Critical";
+                    break;
+            }
+            rowData[orderPriorityIndex] = orderPriority;
+            // calculate 'gross margin' column
+            double totalProfit = Double.parseDouble(rowData[totalProfitIndex]);
+            double totalRevenue = Double.parseDouble(rowData[totalRevenueIndex]);
+            double grossMargin = totalProfit / totalRevenue;
+            rowData = Arrays.copyOf(rowData, rowData.length + 1);
+            rowData[rowData.length - 1] = String.format("%.2f",grossMargin);
+            // remove duplicate row based on the 'OrderID' column by checking the orderID hashset
+            if (orderID.contains(rowData[orderIdIndex])) {
+                continue;
+            } else {
+                orderID.add(rowData[orderIdIndex]);
+                
+            }
+            String newLine = String.join(",", rowData);
+            newLine += "\n";
+            sw.append(newLine);
             // code to perform the necessary transformation
             
         }
         scanner.close();
-        
-
+        // write the transformed content to a new file on s3
         byte[] bytes = sw.toString().getBytes(StandardCharsets.UTF_8); 
         InputStream is = new ByteArrayInputStream(bytes); 
         ObjectMetadata meta = new ObjectMetadata(); 
@@ -79,7 +155,7 @@ public class ProcessCSV implements RequestHandler<Request, HashMap<String, Objec
         String transformedFile = "transformed-" + filename;
         String outputBucketname = "transformed-csv";
         s3Client.putObject(outputBucketname, transformedFile, is, meta);
-        LambdaLogger logger = context.getLogger();
+        
         logger.log("ProcessCSV in bucketname:" + outputBucketname + ", filename after transformaiton:" + transformedFile );
         
         

e830b8a8a91c35a5030188061ef65b0cd50ac9ef
Brucegyx <yexuan.gao@outlook.com>
2023-12-08 15:11:20 -0800

    reverted handler

--- client/syncCall.sh
+++ client/syncCall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # JSON object to pass to Lambda Function
-json={"\"row\"":50,"\"col\"":10,"\"bucketname\"":\"test.bucket.462562f23.yx\"","\"filename\"":\"test.csv\""}
-echo "Invoking Lambda function using API Gateway"
-time output=`curl -s -H "Content-Type: application/json" -X POST -d $json https://l8ges5uu0h.execute-api.us-east-2.amazonaws.com/Transform`
-echo ""
-echo "JSON RESULT:"
-echo $output | jq
-echo ""
+# json={"\"row\"":50,"\"col\"":10,"\"bucketname\"":\"test.bucket.462562f23.yx\"","\"filename\"":\"test.csv\""}
+# echo "Invoking Lambda function using API Gateway"
+# time output=`curl -s -H "Content-Type: application/json" -X POST -d $json https://l8ges5uu0h.execute-api.us-east-2.amazonaws.com/Transform`
+# echo ""
+# echo "JSON RESULT:"
+# echo $output | jq
+# echo ""
 ###### calling service 2 ######
 # JSON object to pass to Lambda Function
 json={"\"name\"":"\"Susan\u0020Smith\",\"param1\"":1,\"param2\"":2,\"param3\"":3}

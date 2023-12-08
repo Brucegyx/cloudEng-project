239735e10b5270591c4e8f888f9992152955a425
Brucegyx <yexuan.gao@outlook.com>
2023-12-08 14:54:56 -0800

    added local api call handler

--- client/syncCall.sh
+++ client/syncCall.sh
@@ -2,7 +2,7 @@
 # JSON object to pass to Lambda Function
 json={"\"row\"":50,"\"col\"":10,"\"bucketname\"":\"test.bucket.462562f23.yx\"","\"filename\"":\"test.csv\""}
 echo "Invoking Lambda function using API Gateway"
-time output=`curl -s -H "Content-Type: application/json" -X POST -d $json https://rvhiz7dt8e.execute-api.us-east-2.amazonaws.com/create_csv`
+time output=`curl -s -H "Content-Type: application/json" -X POST -d $json https://l8ges5uu0h.execute-api.us-east-2.amazonaws.com/Transform`
 echo ""
 echo "JSON RESULT:"
 echo $output | jq

1d12adda23693daa1c17d13d9754fb8150de3e60
Christopher <cmh6415@uw.edu>
2023-12-13 04:45:13 -0800

    MySQL

--- AuroraRDSLambda/java_template/test/callservice.sh
+++ AuroraRDSLambda/java_template/test/callservice.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # JSON object to pass to Lambda Function
-json={"\"name\"":"\"Susan\u0020Smith\",\"param1\"":1,\"param2\"":2,\"param3\"":3}
+json={"\"name\"":"\"Tommy\u0020Tomson\",\"param1\"":1,\"param2\"":2,\"param3\"":3}
 
 #echo "Invoking Lambda function using API Gateway"
 #time output=`curl -s -H "Content-Type: application/json" -X POST -d $json {API-GATEWAY-REST-URL}`

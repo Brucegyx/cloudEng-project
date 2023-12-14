f18cbbfb51fb914dc6b23080d64c9ec212dca6e6
Brucegyx <yexuan.gao@outlook.com>
2023-12-14 10:36:24 -0800

    update to api invoking

--- service3/saaf_sqlite3_query/test/callservice.sh
+++ service3/saaf_sqlite3_query/test/callservice.sh
@@ -4,11 +4,11 @@
 filenameWithoutSuffix=${1%.csv}   # remove suffix from filename
 dbFile="transformed-$filenameWithoutSuffix.db"
 echo $dbFile
-json={"\"dbBucket\"":"\"sqlite-db562\"","\"dbFile\"":"\"$dbFile\"","\"aggregation\"":"\"AVG(GrossMargin) \"","\"filter\"":"\"\""}
+json={"\"dbBucket\"":"\"sqlite-db562\"","\"dbFile\"":"\"$dbFile\"","\"aggregation\"":"\"MAX(UnitsSold)\"","\"filter\"":"\"\""}
 echo "Invoking Service 3 using API Gateway"
 echo $json | jq
-#time output=`curl -s -H "Content-Type: application/json" -X POST -d "$json" https://db4m5x9c3a.execute-api.us-east-2.amazonaws.com/query`
-time output=`aws lambda invoke --invocation-type RequestResponse --function-name Service3 --region us-east-2 --payload "$json" /dev/stdout | head -n 1 | head -c -2 ; echo`
+time output=`curl -s -H "Content-Type: application/json" -X POST -d "$json" https://db4m5x9c3a.execute-api.us-east-2.amazonaws.com/query`
+#time output=`aws lambda invoke --invocation-type RequestResponse --function-name Service3 --region us-east-2 --payload "$json" /dev/stdout | head -n 1 | head -c -2 ; echo`
 
 echo "Service 3 JSON RESULT:"
 echo $output | jq
